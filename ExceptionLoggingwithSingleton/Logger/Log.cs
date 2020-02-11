ac15400df4934caf307a3156b6fb87bfb09a0c74
MYComodo <my956024@gmail.com>
2020-02-11 10:46:51 +0300

    Exception logger

---
+++ ExceptionLoggingwithSingleton/Logger/Log.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Logger
+{
+    public sealed class Log : ILog
+    {
+        
+        private static Log instance = null;
+        private static readonly object obj = new object();
+        public static Log GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (obj)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Log();
+
+                        }
+                    }
+                }
+
+
+                return instance;
+            }
+        }
+        private Log()
+        {
+           
+        }
+
+        public void LogException(string message)
+        {
+            string fileName = string.Format("{0}_{1}", "Exception", DateTime.Now.ToShortDateString());
+            string logFilePath = string.Format(@"{0}\{1}", AppDomain.CurrentDomain.BaseDirectory, fileName);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("----------------------------------------");
+            stringBuilder.AppendLine(DateTime.Now.ToString());
+            stringBuilder.AppendLine(message);
+
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                writer.Write(stringBuilder.ToString());
+                writer.Flush();
+            }
+        }
+
+
+
+        //public class DerivatedSingleton : Singleton
+        //{
+
+        //}
+    }
+}
